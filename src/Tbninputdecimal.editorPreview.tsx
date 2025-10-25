a37e2be0bc4a9e68564bd245d7b2f1d5e4f6c6f9
Tony Tran <quangn@stsafrica.com>
2025-10-25 10:40:23 +0700

    Initial widget scaffolding

---
+++ src/Tbninputdecimal.editorPreview.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, createElement } from "react";
+import { HelloWorldSample } from "./components/HelloWorldSample";
+import { TbninputdecimalPreviewProps } from "../typings/TbninputdecimalProps";
+
+export function preview({ sampleText }: TbninputdecimalPreviewProps): ReactElement {
+    return <HelloWorldSample sampleText={sampleText} />;
+}
+
+export function getPreviewCss(): string {
+    return require("./ui/Tbninputdecimal.css");
+}
---
+++ src/Tbninputdecimal.tsx
@@ -0,0 +1,10 @@
+import { ReactElement, createElement } from "react";
+import { HelloWorldSample } from "./components/HelloWorldSample";
+
+import { TbninputdecimalContainerProps } from "../typings/TbninputdecimalProps";
+
+import "./ui/Tbninputdecimal.css";
+
+export function Tbninputdecimal({ sampleText }: TbninputdecimalContainerProps): ReactElement {
+    return <HelloWorldSample sampleText={sampleText ? sampleText : "World"} />;
+}
